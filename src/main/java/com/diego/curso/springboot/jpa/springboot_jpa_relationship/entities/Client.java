2a534de29d6f3e8e51fddb1fcf3ef9308e7fb2c0
Diego Armando <fordiego44@gmail.com>
2025-04-13 20:19:24 -0500

    OneToMany OneToOne ManyToMany

---
+++ src/main/java/com/diego/curso/springboot/jpa/springboot_jpa_relationship/entities/Client.java
@@ -0,0 +1,130 @@
+package com.diego.curso.springboot.jpa.springboot_jpa_relationship.entities;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
+
+@Entity
+@Table(name = "clients")
+public class Client {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String lastname;
+
+    // @JoinColumn(name = "client_id")
+    @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true)
+    @JoinTable(
+        name = "tbl_clientes_to_direcciones", 
+        joinColumns = @JoinColumn(name= "id_cliente"),
+        inverseJoinColumns = @JoinColumn(name="id_direcciones"),
+        uniqueConstraints = @UniqueConstraint(columnNames = {"id_direcciones"})) 
+    private Set<Address> addresses;
+
+    @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true, mappedBy="client")
+    private List<Invoice> invoices;
+
+    @OneToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL, orphanRemoval = true, mappedBy = "client")
+    private ClientDetails clientDetails;
+
+    public Client() {
+        this.addresses = new HashSet<>();
+        invoices = new ArrayList<>();
+    }
+ 
+    public Client(String name, String lastname) {
+        this();
+        this.name = name;
+        this.lastname = lastname;
+    }
+  
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getLastname() {
+        return lastname;
+    }
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public Set<Address> getAddresses() {
+        return addresses;
+    }
+
+    public void setAddresses(Set<Address> addresses) {
+        this.addresses = addresses;
+    }
+    
+    public List<Invoice> getInvoices() {
+        return invoices;
+    }
+
+    public void setInvoices(List<Invoice> invoices) {
+        this.invoices = invoices;
+    }
+
+    public Client addInvoice(Invoice invoice) {
+        invoices.add(invoice);
+        invoice.setClient(this);
+        return this;
+    } 
+
+    public void removeInvoice(Invoice invoice) {
+        this.getInvoices().remove(invoice);
+        invoice.setClient(null);
+    }
+
+    public ClientDetails getClientDetails() {
+        return clientDetails;
+    }
+
+    public void setClientDetails(ClientDetails clientDetails) {
+        this.clientDetails = clientDetails;
+        clientDetails.setClient(this);
+    }
+
+    public void removeClientDetails(ClientDetails clientDetails) {
+        clientDetails.setClient(null);
+        this.clientDetails = null;
+    }
+    
+    @Override
+    public String toString() {
+        return "{id=" + id +
+                ", name=" + name +
+                ", lastname=" + lastname +
+                ", invoices=" + invoices +
+                ", addresses=" + addresses +
+                ", clientDetails=" + clientDetails +
+                 "}";
+    }
+
+
+    
+}
---
+++ src/main/java/com/diego/curso/springboot/jpa/springboot_jpa_relationship/repositories/ClientDetailsRepository.java
@@ -0,0 +1,9 @@
+package com.diego.curso.springboot.jpa.springboot_jpa_relationship.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.diego.curso.springboot.jpa.springboot_jpa_relationship.entities.ClientDetails;
+
+public interface ClientDetailsRepository extends CrudRepository<ClientDetails, Long>  {
+
+}
